98c41f5aace2a012304dd8eb373b3556fd904470
Canmi <9997200@qq.com>
2025-12-26 14:57:57 +0800

    remove: http

--- src/main.rs
+++ src/main.rs
@@ -7,12 +7,7 @@ pub(crate) mod common {
             System(),
             Anyhow(),
         }
-        impl std::fmt::Display for Error {
-            fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-                loop {}
-            }
-        }
-        impl std::error::Error for Error {}
+
         pub(crate) type Result<T> = std::result::Result<T, Error>;
     }
 }
@@ -22,63 +17,68 @@ pub(crate) mod modules {
         pub(crate) mod protocol {
             pub(crate) mod application {
                 pub(crate) mod http {
-                    // MOCKED HYPER TYPES
                     pub(crate) mod mock_hyper {
-                        use std::future::Future;
                         use std::pin::Pin;
-                        use std::task::{Context, Poll};
-                        use tokio::io::{AsyncRead, AsyncWrite, ReadBuf};
+                        use std::task::Context;
+                        use std::task::Poll;
+                        use tokio::io::AsyncRead;
+                        use tokio::io::AsyncWrite;
+                        use tokio::io::ReadBuf;
 
-                        // The culprit: Upgraded is Send but !Sync
-                        pub struct Upgraded {
-                            // Box<dyn Trait + Send> is NOT Sync
-                            pub io: Box<dyn AsyncReadWrite + Send>,
+                        pub(crate) struct Upgraded {
+                            pub(crate) io: Box<dyn AsyncReadWrite + Send>,
                         }
 
-                        pub trait AsyncReadWrite: AsyncRead + AsyncWrite + Unpin {}
+                        pub(crate) trait AsyncReadWrite:
+                            AsyncRead + AsyncWrite + Unpin
+                        {
+                        }
+
                         impl<T: AsyncRead + AsyncWrite + Unpin> AsyncReadWrite for T {}
 
                         impl AsyncRead for Upgraded {
                             fn poll_read(
-                                mut self: Pin<&mut Self>,
-                                cx: &mut Context<'_>,
-                                buf: &mut ReadBuf<'_>,
+                                self: Pin<&mut Self>,
+                                _cx: &mut Context<'_>,
+                                _buf: &mut ReadBuf<'_>,
                             ) -> Poll<std::io::Result<()>> {
-                                Pin::new(&mut self.io).poll_read(cx, buf)
+                                loop {}
                             }
                         }
 
                         impl AsyncWrite for Upgraded {
                             fn poll_write(
-                                mut self: Pin<&mut Self>,
-                                cx: &mut Context<'_>,
-                                buf: &[u8],
+                                self: Pin<&mut Self>,
+                                _cx: &mut Context<'_>,
+                                _buf: &[u8],
                             ) -> Poll<std::io::Result<usize>> {
-                                Pin::new(&mut self.io).poll_write(cx, buf)
+                                loop {}
                             }
+
                             fn poll_flush(
-                                mut self: Pin<&mut Self>,
-                                cx: &mut Context<'_>,
+                                self: Pin<&mut Self>,
+                                _cx: &mut Context<'_>,
                             ) -> Poll<std::io::Result<()>> {
-                                Pin::new(&mut self.io).poll_flush(cx)
+                                loop {}
                             }
+
                             fn poll_shutdown(
-                                mut self: Pin<&mut Self>,
-                                cx: &mut Context<'_>,
+                                self: Pin<&mut Self>,
+                                _cx: &mut Context<'_>,
                             ) -> Poll<std::io::Result<()>> {
-                                Pin::new(&mut self.io).poll_shutdown(cx)
+                                loop {}
                             }
                         }
 
-                        pub type OnUpgrade =
+                        pub(crate) type OnUpgrade =
                             Pin<Box<dyn Future<Output = Result<Upgraded, ()>> + Send>>;
 
-                        pub struct Incoming; // Dummy
+                        pub(crate) struct Incoming;
                     }
 
                     pub(crate) mod wrapper {
-                        use super::mock_hyper::{Incoming, OnUpgrade};
-                        use std::future::Future;
+                        use super::mock_hyper::Incoming;
+                        use super::mock_hyper::OnUpgrade;
                         use std::pin::Pin;
 
                         pub(crate) enum VaneBody {
@@ -93,73 +93,65 @@ pub(crate) mod modules {
                     }
 
                     pub(crate) mod httpx {
-                        use super::mock_hyper::{Incoming, Upgraded};
+                        use super::mock_hyper::Incoming;
+                        use super::mock_hyper::Upgraded;
                         use super::wrapper::VaneBody;
                         use crate::common::requirements::Error;
                         use crate::common::requirements::Result;
-                        use crate::modules::stack::protocol::application::container::{
-                            Container, PayloadState,
-                        };
-                        use http::{Request, Response};
-                        use tokio::io::{AsyncRead, AsyncWrite};
+                        use crate::modules::stack::protocol::application::container::Container;
+                        use crate::modules::stack::protocol::application::container::PayloadState;
+                        use tokio::io::AsyncRead;
+                        use tokio::io::AsyncWrite;
                         use tokio::sync::oneshot;
+
+                        pub(crate) struct Request<T>(pub T);
+
+                        pub(crate) struct Response<T>(pub T);
 
                         pub(crate) trait ByteStream:
                             AsyncRead + AsyncWrite + Unpin + Send + Sync
                         {
                         }
-                        impl<T: AsyncRead + AsyncWrite + Unpin + Send + Sync> ByteStream for T {}
 
-                        // The outer future structure
                         pub(crate) async fn handle_connection(
                             _conn: Box<dyn ByteStream>,
                             protocol_id: String,
                         ) -> Result<()> {
-                            // Mocking the service_fn structure
                             let service = move |req: Request<Incoming>| {
                                 let proto = protocol_id.clone();
                                 async move { serve_request(req, proto).await }
                             };
-
-                            // Mocking the serve loop
-                            let _ = service(Request::new(Incoming)).await;
+                            let _ = service(Request(Incoming)).await;
                             Ok(())
                         }
 
                         async fn dummy_flow_step(_container: &mut Container) {
-                            tokio::task::yield_now().await;
+                            loop {}
                         }
 
                         async fn serve_request(
-                            mut req: Request<Incoming>,
+                            req: Request<Incoming>,
                             _protocol_id: String,
                         ) -> std::result::Result<Response<Incoming>, Error>
                         {
-                            let client_upgrade_handle =
-                                if req.headers().contains_key(http::header::UPGRADE) {
-                                    // Mocking hyper::upgrade::on
-                                    Some(Box::pin(async {
-                                        Ok(Upgraded {
-                                            io: Box::new(tokio::io::empty()),
-                                        })
+                            let client_upgrade_handle = if true {
+                                Some(Box::pin(async {
+                                    Ok(Upgraded {
+                                        io: Box::new(tokio::io::empty()),
                                     })
-                                        as super::mock_hyper::OnUpgrade)
-                                } else {
-                                    None
-                                };
-
-                            let (mut parts, _body) = req.into_parts();
+                                })
+                                    as super::mock_hyper::OnUpgrade)
+                            } else {
+                                None
+                            };
+                            let _body = req.0;
                             let request_payload = PayloadState::Http(VaneBody::Hyper(Incoming));
                             let response_payload = PayloadState::Empty;
                             let (res_tx, res_rx) = oneshot::channel::<Response<()>>();
-
                             let mut container =
                                 Container::new(request_payload, response_payload, Some(res_tx));
                             container.client_upgrade = client_upgrade_handle;
-                            let _ = parts.headers;
-
                             dummy_flow_step(&mut container).await;
-
                             match res_rx.await {
                                 Ok(_response_parts) => {
                                     let mut payload = std::mem::replace(
@@ -173,19 +165,13 @@ pub(crate) mod modules {
                                         if let Some(client_upgrade) =
                                             container.client_upgrade.take()
                                         {
-                                            // ICE Trigger Zone
                                             let tunnel_future = Box::pin(async move {
                                                 tokio::task::yield_now().await;
-                                                // Upgraded is !Sync.
-                                                // try_join awaits both.
-                                                // The resulting future holds !Sync state across yield points.
                                                 match tokio::try_join!(
                                                     client_upgrade,
                                                     upstream_upgrade
                                                 ) {
                                                     Ok((mut client_io, mut upstream_io)) => {
-                                                        // copy_bidirectional requires AsyncRead+AsyncWrite
-                                                        // Our Upgraded implements it.
                                                         let _ = tokio::io::copy_bidirectional(
                                                             &mut client_io,
                                                             &mut upstream_io,
@@ -195,8 +181,6 @@ pub(crate) mod modules {
                                                     Err(_) => {}
                                                 }
                                             });
-
-                                            // ICE: coercing !Sync future to Sync trait object
                                             payload = PayloadState::Http(VaneBody::UpgradeBridge {
                                                 tunnel_task: Some(tunnel_future),
                                             });
@@ -213,9 +197,9 @@ pub(crate) mod modules {
                 }
 
                 pub(crate) mod container {
+                    use crate::modules::stack::protocol::application::http::httpx::Response;
                     use crate::modules::stack::protocol::application::http::mock_hyper::OnUpgrade;
                     use crate::modules::stack::protocol::application::http::wrapper::VaneBody;
-                    use http::Response;
                     use tokio::sync::oneshot;
 
                     pub(crate) enum PayloadState {
