3b489ece2c069a950553b223324acba88f48f15d
Canmi <9997200@qq.com>
2025-12-26 14:40:44 +0800

    chore: simply flow::execute_l7

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+/* src/main.rs */
+
 pub(crate) mod common {
     pub(crate) mod requirements {
         #[derive(Debug)]
@@ -5,62 +7,38 @@ pub(crate) mod common {
             System(),
             Anyhow(),
         }
-
         impl std::fmt::Display for Error {
             fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-                loop { }
+                loop {}
             }
         }
-
-        impl std::error::Error for Error { }
-
+        impl std::error::Error for Error {}
         pub(crate) type Result<T> = std::result::Result<T, Error>;
     }
 }
 
 pub(crate) mod modules {
-    pub(crate) mod plugins {
-        pub(crate) mod model {
-            use tokio::io::AsyncRead;
-            use tokio::io::AsyncWrite;
-
-            #[derive(Debug, Clone, PartialEq, Eq)]
-            pub(crate) struct ProcessingStep;
-
-            impl ProcessingStep {
-                pub(crate) fn new() -> Self {
-                    loop { }
-                }
-            }
-
-            pub(crate) trait ByteStream: AsyncRead + AsyncWrite + Unpin + Send + Sync { }
-
-            pub(crate) enum ConnectionObject {
-                Stream(Box<dyn ByteStream>),
-            }
-
-            #[derive(Debug)]
-            pub(crate) enum TerminatorResult {
-                Finished,
-                Upgrade {},
-            }
-        }
-    }
+    // REMOVED: pub(crate) mod plugins { ... }
+    // We will inline the minimal IO trait needed for handle_connection below
 
     pub(crate) mod stack {
         pub(crate) mod protocol {
             pub(crate) mod application {
+                // REMOVED: pub(crate) mod flow { ... }
+
                 pub(crate) mod http {
                     pub(crate) mod wrapper {
                         use hyper::body::Incoming;
                         use hyper::upgrade::OnUpgrade;
+                        use std::future::Future;
                         use std::pin::Pin;
 
                         pub(crate) enum VaneBody {
                             Hyper(Incoming),
                             SwitchingProtocols(OnUpgrade),
                             UpgradeBridge {
-                                tunnel_task: Option<Pin<Box<dyn Future<Output = ()> + Send + Sync>>>,
+                                tunnel_task:
+                                    Option<Pin<Box<dyn Future<Output = ()> + Send + Sync>>>,
                             },
                             Empty,
                         }
@@ -70,105 +48,129 @@ pub(crate) mod modules {
                         use super::wrapper::VaneBody;
                         use crate::common::requirements::Error;
                         use crate::common::requirements::Result;
-                        use crate::modules::plugins::model::ConnectionObject;
-                        use crate::modules::plugins::model::ProcessingStep;
-                        use crate::modules::stack::protocol::application::container::Container;
-                        use crate::modules::stack::protocol::application::container::PayloadState;
-                        use crate::modules::stack::protocol::application::flow;
+                        use crate::modules::stack::protocol::application::container::{
+                            Container, PayloadState,
+                        };
                         use bytes::Bytes;
-                        use http::Request;
-                        use http::Response;
+                        use http::{Request, Response};
                         use http_body_util::combinators::BoxBody;
                         use hyper::body::Incoming;
                         use hyper::service::service_fn;
                         use hyper_util::rt::TokioIo;
                         use hyper_util::server::conn::auto::Builder as AutoBuilder;
+                        use tokio::io::{AsyncRead, AsyncWrite};
                         use tokio::sync::oneshot;
 
+                        // Local definition to replace plugins::model::ByteStream
+                        pub(crate) trait ByteStream:
+                            AsyncRead + AsyncWrite + Unpin + Send + Sync
+                        {
+                        }
+                        impl<T: AsyncRead + AsyncWrite + Unpin + Send + Sync> ByteStream for T {}
+
+                        // Modified: Takes Box<dyn ByteStream> directly, removing ConnectionObject wrapper
                         pub(crate) async fn handle_connection(
-                            conn: ConnectionObject,
+                            conn: Box<dyn ByteStream>,
                             protocol_id: String,
                         ) -> Result<()> {
-                            let io = match conn {
-                                ConnectionObject::Stream(boxed_stream) => {
-                                    TokioIo::new(boxed_stream)
-                                },
-                            };
+                            let io = TokioIo::new(conn);
                             let service = service_fn(move |req: Request<Incoming>| {
                                 let proto = protocol_id.clone();
-                                async move {
-                                    serve_request(req, proto).await
-                                }
+                                async move { serve_request(req, proto).await }
                             });
                             let builder = AutoBuilder::new(hyper_util::rt::TokioExecutor::new());
                             let _ = builder.serve_connection(io, service).await;
                             Ok(())
                         }
 
+                        // Dummy replacement for flow::execute_l7
+                        // Keeps the signature similiar (borrowing container) and the await point
+                        async fn dummy_flow_step(_container: &mut Container) {
+                            tokio::task::yield_now().await;
+                        }
+
                         async fn serve_request(
                             mut req: Request<Incoming>,
                             _protocol_id: String,
-                        ) -> std::result::Result<Response<BoxBody<Bytes, Error>>, Error> {
-                            let client_upgrade_handle = if req.headers().contains_key(http::header::UPGRADE) {
-                                Some(hyper::upgrade::on(&mut req))
-                            } else {
-                                None
-                            };
+                        ) -> std::result::Result<Response<BoxBody<Bytes, Error>>, Error>
+                        {
+                            let client_upgrade_handle =
+                                if req.headers().contains_key(http::header::UPGRADE) {
+                                    Some(hyper::upgrade::on(&mut req))
+                                } else {
+                                    None
+                                };
                             let (mut parts, body) = req.into_parts();
                             let request_payload = PayloadState::Http(VaneBody::Hyper(body));
                             let response_payload = PayloadState::Empty;
                             let (res_tx, res_rx) = oneshot::channel::<Response<()>>();
-                            let mut container = Container::new(request_payload, response_payload, Some(res_tx));
+
+                            let mut container =
+                                Container::new(request_payload, response_payload, Some(res_tx));
                             container.client_upgrade = client_upgrade_handle;
                             let _ = parts.headers;
-                            let pipeline = ProcessingStep::new();
-                            if let Err(_e) = flow::execute_l7(&pipeline, &mut container, "".to_string()).await {
-                                return Ok(response_error());
-                            }
+
+                            // REMOVED: Pipeline creation
+                            // CHANGED: flow::execute_l7 -> dummy_flow_step
+                            dummy_flow_step(&mut container).await;
+
                             match res_rx.await {
                                 Ok(response_parts) => {
                                     let (parts, _) = response_parts.into_parts();
-                                    let mut payload =
-                                        std::mem::replace(&mut container.response_body, PayloadState::Empty);
-                                    if let PayloadState::Http(VaneBody::SwitchingProtocols(upstream_upgrade)) =
-                                        payload {
-                                        if let Some(client_upgrade) = container.client_upgrade.take() {
+                                    let mut payload = std::mem::replace(
+                                        &mut container.response_body,
+                                        PayloadState::Empty,
+                                    );
+                                    if let PayloadState::Http(VaneBody::SwitchingProtocols(
+                                        upstream_upgrade,
+                                    )) = payload
+                                    {
+                                        if let Some(client_upgrade) =
+                                            container.client_upgrade.take()
+                                        {
+                                            // ICE Trigger Zone
                                             let tunnel_future = Box::pin(async move {
                                                 tokio::task::yield_now().await;
-                                                match tokio::try_join!(client_upgrade, upstream_upgrade) {
+                                                match tokio::try_join!(
+                                                    client_upgrade,
+                                                    upstream_upgrade
+                                                ) {
                                                     Ok((mut client_io, mut upstream_io)) => {
-                                                        let mut client_tokio = TokioIo::new(&mut client_io);
-                                                        let mut upstream_tokio = TokioIo::new(&mut upstream_io);
-                                                        let _ =
-                                                            tokio::io::copy_bidirectional(
-                                                                &mut client_tokio,
-                                                                &mut upstream_tokio,
-                                                            ).await;
-                                                    },
-                                                    Err(_) => { },
+                                                        let mut client_tokio =
+                                                            TokioIo::new(&mut client_io);
+                                                        let mut upstream_tokio =
+                                                            TokioIo::new(&mut upstream_io);
+                                                        let _ = tokio::io::copy_bidirectional(
+                                                            &mut client_tokio,
+                                                            &mut upstream_tokio,
+                                                        )
+                                                        .await;
+                                                    }
+                                                    Err(_) => {}
                                                 }
                                             });
-                                            payload =
-                                                PayloadState::Http(
-                                                    VaneBody::UpgradeBridge { tunnel_task: Some(tunnel_future) },
-                                                );
+                                            payload = PayloadState::Http(VaneBody::UpgradeBridge {
+                                                tunnel_task: Some(tunnel_future),
+                                            });
                                         } else {
                                             payload = PayloadState::Empty;
                                         }
                                     }
                                     let final_body = convert_payload_to_body(payload);
                                     Ok(Response::from_parts(parts, final_body))
-                                },
+                                }
                                 Err(_) => Ok(response_error()),
                             }
                         }
 
-                        pub(super) fn convert_payload_to_body(_payload: PayloadState) -> BoxBody<Bytes, Error> {
-                            loop { }
+                        pub(super) fn convert_payload_to_body(
+                            _payload: PayloadState,
+                        ) -> BoxBody<Bytes, Error> {
+                            loop {}
                         }
 
                         fn response_error() -> Response<BoxBody<Bytes, Error>> {
-                            loop { }
+                            loop {}
                         }
                     }
                 }
@@ -195,22 +197,8 @@ pub(crate) mod modules {
                             response_body: PayloadState,
                             _response_tx: Option<oneshot::Sender<Response<()>>>,
                         ) -> Self {
-                            loop { }
+                            loop {}
                         }
-                    }
-                }
-
-                pub(crate) mod flow {
-                    use super::container::Container;
-                    use crate::modules::plugins::model::ProcessingStep;
-                    use crate::modules::plugins::model::TerminatorResult;
-
-                    pub(crate) async fn execute_l7(
-                        _step: &ProcessingStep,
-                        _container: &mut Container,
-                        _parent_path: String,
-                    ) -> Result<TerminatorResult, String> {
-                        loop { }
                     }
                 }
             }
@@ -220,5 +208,5 @@ pub(crate) mod modules {
 
 #[tokio::main]
 async fn main() {
-    loop { }
+    loop {}
 }
